dbcce7367739bba4fee75ad397f1ffe803fc291e
André Fatia <andre@thecodeventure.com>
2023-05-13 00:18:35 +0100

    WIP

---
+++ src/SafeSDKPlugin.ts
@@ -0,0 +1,146 @@
+import { ethers } from 'ethers';
+import { SafeTransactionDataPartial } from '@safe-global/safe-core-sdk-types';
+import { SafeAuthKit, Web3AuthModalPack } from '@safe-global/auth-kit';
+import { Web3AuthOptions } from '@web3auth/modal';
+import { OpenloginAdapter } from '@web3auth/openlogin-adapter';
+import { CHAIN_NAMESPACES, WALLET_ADAPTERS } from '@web3auth/base';
+import Safe, { EthersAdapter, SafeFactory } from '@safe-global/protocol-kit';
+
+
+class SafeSDKPlugin {
+  signer: any;
+  safeAuthKit: any;
+
+	async init(
+		web3AuthClientId: string,
+		chainId: string,
+		rpcUrl: string,
+		// use_torus_evm: boolean,
+		// use_metamask: boolean
+	) {
+		// https://web3auth.io/docs/sdk/web/modal/initialize#arguments
+		const options: Web3AuthOptions = {
+			clientId: web3AuthClientId,
+			web3AuthNetwork: 'testnet',
+			chainConfig: {
+				chainNamespace: CHAIN_NAMESPACES.EIP155,
+				chainId: chainId,
+				// https://chainlist.org/
+				rpcTarget: rpcUrl,
+			},
+			uiConfig: {
+				theme: 'dark',
+				loginMethodsOrder: ['google', 'facebook'],
+			},
+		};
+
+		// https://web3auth.io/docs/sdk/web/modal/initialize#configuring-adapters
+		const modalConfig = {
+			[WALLET_ADAPTERS.TORUS_EVM]: {
+				label: 'torus',
+				showOnModal: false,
+			},
+			[WALLET_ADAPTERS.METAMASK]: {
+				label: 'metamask',
+				showOnDesktop: true,
+				showOnMobile: false,
+			},
+		};
+
+		// https://web3auth.io/docs/sdk/web/modal/whitelabel#whitelabeling-while-modal-initialization
+		const openloginAdapter = new OpenloginAdapter({
+			loginSettings: {
+				mfaLevel: 'mandatory',
+			},
+			adapterSettings: {
+				uxMode: 'popup',
+				whiteLabel: {
+					name: 'Safe',
+				},
+			},
+		});
+
+		const pack = new Web3AuthModalPack(
+			options,
+			[openloginAdapter],
+			modalConfig
+		);
+
+		this.safeAuthKit = await SafeAuthKit.init(pack, {
+			txServiceUrl: 'https://safe-transaction-goerli.safe.global',
+		});
+	}
+
+	async signIn() {
+		await this.safeAuthKit.signIn();
+	}
+
+	async signOut() {
+		await this.safeAuthKit.signOut();
+	}
+
+	async createTransaction(
+		destination: string,
+		amount: string,
+		safeAddress: any
+	) {
+		// Any address can be used. In this example you will use vitalik.eth
+		// const destination = '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045';
+    const provider = new ethers.providers.Web3Provider(
+			this.safeAuthKit.getProvider()
+		);
+
+		const signer = provider.getSigner();
+
+		const amountToSend = ethers.utils.parseUnits(amount, 'ether').toString();
+
+		const ethAdapter = new EthersAdapter({
+			ethers,
+			signerOrProvider: signer || provider,
+		});
+
+		const safeSDK = await Safe.create({
+			ethAdapter,
+			safeAddress,
+		});
+
+		const safeTransactionData: SafeTransactionDataPartial = {
+			to: destination,
+			data: '0x',
+			value: amountToSend,
+		};
+
+		// Create a Safe transaction with the provided parameters
+		await safeSDK.createTransaction({
+			safeTransactionData,
+		});
+	}
+
+	async createSafe() {
+    const provider = new ethers.providers.Web3Provider(
+			this.safeAuthKit.getProvider()
+		);
+
+    const owner = provider.getSigner();
+
+    if (!owner) {
+      throw new Error('You are not connected to a wallet');
+    }
+
+    console.log('owner', owner);
+
+    const ethAdapter = new EthersAdapter({
+      ethers,
+      signerOrProvider: owner,
+    });
+
+    const safeFactory = await SafeFactory.create({ ethAdapter: ethAdapter })
+
+    // const safeAddress = await safeFactory.deploySafe({
+    //   owners: [await owner.getAddress()],
+    //   threshold: 1,
+    // });
+  }
+}
+
+export default SafeSDKPlugin
--- src/main.ts
+++ src/main.ts
@@ -1,149 +1,25 @@
-import { ThirdwebSDK } from '@thirdweb-dev/sdk';
-import { ethers } from 'ethers';
-import { SafeTransactionDataPartial } from '@safe-global/safe-core-sdk-types';
-import { SafeAuthKit, Web3AuthModalPack } from '@safe-global/auth-kit';
-import { Web3AuthOptions } from '@web3auth/modal';
-import { OpenloginAdapter } from '@web3auth/openlogin-adapter';
-import { CHAIN_NAMESPACES, WALLET_ADAPTERS } from '@web3auth/base';
-import Safe, { EthersAdapter } from '@safe-global/protocol-kit';
+import SafeSDKPlugin from "./SafeSDKPlugin";
 
 declare global {
 	interface Window {
-		ThirdwebSDK: any;
-		safeAuthKit: any;
-		safeSDKPlugin: any;
+		SafeSDKPlugin: any;
+    runDemo: () => void;
 	}
 }
 
-class SafeSDKPlugin {
-	safeAuthKit: any;
-	web3_auth_client_id: any;
-	chain_id: any;
-	chain_rpc_target: any;
-	use_torus_evm: any;
-	use_metamask: any;
+window.SafeSDKPlugin = SafeSDKPlugin;
 
-	constructor() {}
+async function runDemo() {
+  const web3Auth = 'BIV9bqt-hKSorZvc6Nmng0XlHSc83Dt3kN_-aAH3_CAZPgK3BGTDRJCY7vW--1r9FyMEJh1yAuukv0eOlZk7NMk';
+  const rpc = `https://rpc.ankr.com/eth_goerli`;
 
-	async init(
-		WEB3_AUTH_CLIENT_ID: string,
-		chainId: string,
-		chain_rpc_target: string,
-		use_torus_evm: boolean,
-		use_metamask: boolean
-	) {
-		this.web3_auth_client_id = WEB3_AUTH_CLIENT_ID;
-		this.chain_id = chainId;
-		this.chain_rpc_target = chain_rpc_target;
-		this.use_torus_evm = use_torus_evm;
-		this.use_metamask = use_metamask;
+  const safe = new SafeSDKPlugin();
 
-		// https://web3auth.io/docs/sdk/web/modal/initialize#arguments
-		const options: Web3AuthOptions = {
-			clientId: this.web3_auth_client_id,
-			web3AuthNetwork: 'testnet',
-			chainConfig: {
-				chainNamespace: CHAIN_NAMESPACES.EIP155,
-				chainId: this.chain_id,
-				// https://chainlist.org/
-				rpcTarget: this.chain_rpc_target,
-			},
-			uiConfig: {
-				theme: 'dark',
-				loginMethodsOrder: ['google', 'facebook'],
-			},
-		};
+  await safe.init(web3Auth, '0x5', rpc);
 
-		// https://web3auth.io/docs/sdk/web/modal/initialize#configuring-adapters
-		const modalConfig = {
-			[WALLET_ADAPTERS.TORUS_EVM]: {
-				label: 'torus',
-				showOnModal: false,
-			},
-			[WALLET_ADAPTERS.METAMASK]: {
-				label: 'metamask',
-				showOnDesktop: true,
-				showOnMobile: false,
-			},
-		};
+  await safe.signIn();
 
-		// https://web3auth.io/docs/sdk/web/modal/whitelabel#whitelabeling-while-modal-initialization
-		const openloginAdapter = new OpenloginAdapter({
-			loginSettings: {
-				mfaLevel: 'mandatory',
-			},
-			adapterSettings: {
-				uxMode: 'popup',
-				whiteLabel: {
-					name: 'Safe',
-				},
-			},
-		});
-
-		const pack = new Web3AuthModalPack(
-			options,
-			[openloginAdapter],
-			modalConfig
-		);
-
-		this.safeAuthKit = await SafeAuthKit.init(pack, {
-			txServiceUrl: 'https://safe-transaction-goerli.safe.global',
-		});
-
-		window.ThirdwebSDK = ThirdwebSDK;
-		window.safeAuthKit = this.safeAuthKit;
-	}
-
-	async signIn() {
-		await this.safeAuthKit.signIn();
-	}
-
-	async signOut() {
-		await this.safeAuthKit.signOut();
-	}
-
-	async createTransaction(
-		destination: string,
-		amount: string,
-		safeAddress: any
-	) {
-		// Any address can be used. In this example you will use vitalik.eth
-		// const destination = '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045';
-		const provider = new ethers.providers.Web3Provider(
-			this.safeAuthKit.getProvider()
-		);
-		const signer = provider.getSigner();
-
-		const amountToSend = ethers.utils.parseUnits(amount, 'ether').toString();
-
-		const ethAdapter = new EthersAdapter({
-			ethers,
-			signerOrProvider: signer || provider,
-		});
-
-		const safeSDK = await Safe.create({
-			ethAdapter,
-			safeAddress,
-		});
-
-		const safeTransactionData: SafeTransactionDataPartial = {
-			to: destination,
-			data: '0x',
-			value: amountToSend,
-		};
-		// Create a Safe transaction with the provided parameters
-		await safeSDK.createTransaction({
-			safeTransactionData,
-		});
-	}
-
-	async createSafe() {}
+  await safe.createSafe();
 }
 
-const web3Auth =
-	'BIV9bqt-hKSorZvc6Nmng0XlHSc83Dt3kN_-aAH3_CAZPgK3BGTDRJCY7vW--1r9FyMEJh1yAuukv0eOlZk7NMk';
-const rpc = `https://rpc.ankr.com/eth_goerli`;
-
-const safe = new SafeSDKPlugin();
-safe.init(web3Auth, '0x5', rpc, false, true);
-window.safeSDKPlugin = safe;
+window.runDemo = runDemo;
