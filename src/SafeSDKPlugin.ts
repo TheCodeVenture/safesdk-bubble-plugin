672502c929e00e4f355812999e3d028debc91dee
Joao Martins <joao.martins@thecodeventure.com>
2023-05-13 02:02:39 +0100

    merge createSafe

--- src/SafeSDKPlugin.ts
+++ src/SafeSDKPlugin.ts
@@ -5,16 +5,17 @@ import { Web3AuthOptions } from '@web3auth/modal';
 import { OpenloginAdapter } from '@web3auth/openlogin-adapter';
 import { CHAIN_NAMESPACES, WALLET_ADAPTERS } from '@web3auth/base';
 import Safe, { EthersAdapter, SafeFactory } from '@safe-global/protocol-kit';
-
+import SafeApiKit from '@safe-global/api-kit';
 
 class SafeSDKPlugin {
-  signer: any;
-  safeAuthKit: any;
+	signer: any;
+	safeAuthKit: any;
+	safes: any;
 
 	async init(
 		web3AuthClientId: string,
 		chainId: string,
-		rpcUrl: string,
+		rpcUrl: string
 		// use_torus_evm: boolean,
 		// use_metamask: boolean
 	) {
@@ -73,24 +74,21 @@ class SafeSDKPlugin {
 
 	async signIn() {
 		const res = await this.safeAuthKit.signIn();
-    console.log('res', res);
+		this.safes = res.safes;
+		console.log('res', res);
 	}
 
 	async signOut() {
 		await this.safeAuthKit.signOut();
 	}
 
-	async createTransaction(
-		destination: string,
-		amount: string,
-		safeAddress: any
-	) {
+	async createTransaction(destination: string, amount: string) {
 		// Any address can be used. In this example you will use vitalik.eth
 		// const destination = '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045';
-    const provider = new ethers.providers.Web3Provider(
-			this.safeAuthKit.getProvider()
-		);
-
+		const authKitProvider = this.safeAuthKit.getProvider();
+		const provider = new ethers.providers.Web3Provider(authKitProvider);
+		const safeAddress = this.safes[0];
+		console.log(safeAddress);
 		const signer = provider.getSigner();
 
 		const amountToSend = ethers.utils.parseUnits(amount, 'ether').toString();
@@ -100,64 +98,110 @@ class SafeSDKPlugin {
 			signerOrProvider: signer || provider,
 		});
 
+		console.log(ethAdapter);
+		console.log(safeAddress);
 		const safeSDK = await Safe.create({
 			ethAdapter,
 			safeAddress,
 		});
+
+		console.log(safeSDK);
 
 		const safeTransactionData: SafeTransactionDataPartial = {
 			to: destination,
 			data: '0x',
 			value: amountToSend,
 		};
-
 		// Create a Safe transaction with the provided parameters
-		await safeSDK.createTransaction({
+		const res = await safeSDK.createTransaction({
 			safeTransactionData,
 		});
+
+		const safeSDK2 = await safeSDK.connect({
+			ethAdapter,
+			safeAddress,
+		});
+
+		const txHash = await safeSDK2.getTransactionHash(res);
+		return { res, txHash };
+	}
+
+	async signTransaction(tx: any, message: string) {
+		const provider = new ethers.providers.Web3Provider(
+			this.safeAuthKit.getProvider()
+		);
+		const signer = provider.getSigner();
+
+		await signer.sendTransaction(tx);
+		await signer.signTransaction(tx);
+		await signer.signMessage(message);
+	}
+
+	async getPendingTransactions() {
+		const provider = new ethers.providers.Web3Provider(
+			this.safeAuthKit.getProvider()
+		);
+		const signer = provider.getSigner();
+		// Sign transaction to verify that the transaction is coming from owner 1
+		const ethAdapter = new EthersAdapter({
+			ethers,
+			signerOrProvider: signer || provider,
+		});
+
+		const txServiceUrl = 'https://safe-transaction-goerli.safe.global';
+		const safeService = new SafeApiKit({
+			txServiceUrl,
+			ethAdapter: ethAdapter,
+		});
+
+		const address = this.safes[0];
+		const pendingTransactions = await safeService.getPendingTransactions(
+			address
+		);
+		console.log(pendingTransactions);
 	}
 
 	async createSafe() {
-    const provider = new ethers.providers.Web3Provider(
+		const provider = new ethers.providers.Web3Provider(
 			this.safeAuthKit.getProvider()
 		);
 
-    const owner = provider.getSigner();
+		const owner = provider.getSigner();
 
-    console.log('owner', owner);
+		console.log('owner', owner);
 
-    if (!owner) {
-      throw new Error('You are not connected to a wallet');
-    }
+		if (!owner) {
+			throw new Error('You are not connected to a wallet');
+		}
 
-    const address = await owner.getAddress();
+		const address = await owner.getAddress();
 
-    console.log('address', address);
+		console.log('address', address);
 
-    const ethAdapter = new EthersAdapter({
-      ethers,
-      signerOrProvider: owner,
-    });
+		const ethAdapter = new EthersAdapter({
+			ethers,
+			signerOrProvider: owner,
+		});
 
-    const safeFactory = await SafeFactory.create({ ethAdapter: ethAdapter })
+		const safeFactory = await SafeFactory.create({ ethAdapter: ethAdapter });
 
-    const safeAddress = await safeFactory.deploySafe({
-      safeAccountConfig: {
-        owners: [address],
-        threshold: 1,
-      },
-    });
+		const safeAddress = await safeFactory.deploySafe({
+			safeAccountConfig: {
+				owners: [address],
+				threshold: 1,
+			},
+		});
 
-    console.log('safeAddress', safeAddress);
-  }
+		console.log('safeAddress', safeAddress);
+	}
 
-  async getPrivateKey() {
-    const authKitProvider = this.safeAuthKit.getProvider();
-    const privateKey = await authKitProvider.request({
-        method: "private_key"
-    });
-    console.log('privateKey', privateKey);
-  }
+	async getPrivateKey() {
+		const authKitProvider = this.safeAuthKit.getProvider();
+		const privateKey = await authKitProvider.request({
+			method: 'private_key',
+		});
+		console.log('privateKey', privateKey);
+	}
 }
 
-export default SafeSDKPlugin
+export default SafeSDKPlugin;
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,32 @@
-import SafeSDKPlugin from "./SafeSDKPlugin";
+import SafeSDKPlugin from './SafeSDKPlugin';
 
 declare global {
 	interface Window {
 		SafeSDKPlugin: any;
-    runDemo: () => void;
+		runDemo: () => void;
 	}
 }
 
 window.SafeSDKPlugin = SafeSDKPlugin;
 
 async function runDemo() {
-  const web3Auth = 'BIV9bqt-hKSorZvc6Nmng0XlHSc83Dt3kN_-aAH3_CAZPgK3BGTDRJCY7vW--1r9FyMEJh1yAuukv0eOlZk7NMk';
-  const rpc = `https://rpc.ankr.com/eth_goerli`;
+	const web3Auth =
+		'BIV9bqt-hKSorZvc6Nmng0XlHSc83Dt3kN_-aAH3_CAZPgK3BGTDRJCY7vW--1r9FyMEJh1yAuukv0eOlZk7NMk';
+	const rpc = `https://rpc.ankr.com/eth_goerli`;
 
-  const safe = new SafeSDKPlugin();
+	const safe = new SafeSDKPlugin();
 
-  await safe.init(web3Auth, '0x5', rpc);
+	await safe.init(web3Auth, '0x5', rpc);
 
-  await safe.signIn();
+	await safe.signIn();
 
-  // await safe.createSafe();
+	// await safe.createSafe();
+	const resTx = await safe.createTransaction(
+		'0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045',
+		'0.005'
+	);
+
+	console.log(resTx);
 }
 
 window.runDemo = runDemo;
